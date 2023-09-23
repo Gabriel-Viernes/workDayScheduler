05ab9953cdfc3c66e655b92fad3e4cd61670b248
Gabriel Viernes <viernesgabriel@gmail.com>
2023-09-23 14:35:18 -0500

    added code to convert 24hr times to 12hr, rewrote README

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+// displays current day as: "Today is Tuesday, 01,01,1901"
 $('#currentDay').text('Today is: ' + dayjs().format('dddd, MM/DD/YY'));
+// appends time blocks from 
 $(function loadTimeBlocks () {
   for (let i = 9; i < 18; i++) {
+    let reformat;
+    if(i > 12) {
+      reformat = (i - 12) + ':00 PM'
+    } else {
+      reformat = i + ':00 AM'
+    }
     let timeBlock = $('<div id = "hour-' + i + '" class = "row time-block past" id="test"></div>')
     $('#timeBlockContainer').append(timeBlock)
     if(i < dayjs().format('H')) {
@@ -10,7 +18,7 @@ $(function loadTimeBlocks () {
     } else {
       timeBlock.addClass("future")
     }
-    timeBlock.append('<div class = "col-2 col-md-1 hour text-center py-3">' + i + ':00' + '</div>');
+    timeBlock.append('<div class = "col-2 col-md-1 hour text-center py-3">' + reformat + '</div>');
     if(localStorage.getItem('hour-'+i) != null) {
       timeBlock.append('<textarea class = "col-8 col-md-10 description" rows="3">' + localStorage.getItem('hour-'+i) + '</textarea>');
     } else {
