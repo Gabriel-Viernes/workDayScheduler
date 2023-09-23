982c9af999075292afef08e11bf974b64a6d5885
Gabriel Viernes <viernesgabriel@gmail.com>
2023-09-23 14:04:56 -0500

    loadTimeBlocks now loads events from localStorage if a matching id can be found

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ $(function loadTimeBlocks () {
       timeBlock.addClass("future")
     }
     timeBlock.append('<div class = "col-2 col-md-1 hour text-center py-3">' + i + ':00' + '</div>');
-    timeBlock.append('<textarea class = "col-8 col-md-10 description" rows="3"> </textarea>');
+    if(localStorage.getItem('hour-'+i) != null) {
+      timeBlock.append('<textarea class = "col-8 col-md-10 description" rows="3">' + localStorage.getItem('hour-'+i) + '</textarea>');
+    } else {
+      timeBlock.append('<textarea class = "col-8 col-md-10 description" rows="3"></textarea>');
+    }
     timeBlock.append('<button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i></button>');
   }
 })
