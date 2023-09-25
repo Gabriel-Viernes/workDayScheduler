2ff9ea6fb8fecc8460c15a5aeb347f9a1ed44043
Gabriel Viernes <viernesgabriel@gmail.com>
2023-09-25 10:30:29 -0500

    added comments

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // displays current day as: "Today is Tuesday, 01,01,1901"
 $('#currentDay').text('Today is: ' + dayjs().format('dddd, MM/DD/YY'));
-// appends time blocks from 
+// appends time blocks to site, pulls appropriate data from localStorage and displays it
 $(function loadTimeBlocks () {
   for (let i = 9; i < 18; i++) {
+    // reformats 24h to 12h
     let reformat;
     if(i > 12) {
       reformat = (i - 12) + ':00 PM'
     } else {
       reformat = i + ':00 AM'
     }
+    // appends time blocks to html
     let timeBlock = $('<div id = "hour-' + i + '" class = "row time-block past" id="test"></div>')
     $('#timeBlockContainer').append(timeBlock)
+    // colors blocks depending on current time
     if(i < dayjs().format('H')) {
       timeBlock.addClass("past")
     } else if( i == dayjs().format('H')) {
@@ -27,6 +30,8 @@ $(function loadTimeBlocks () {
     timeBlock.append('<button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i></button>');
   }
 })
+
+// adds event listeners to every save button and saves text in the text area to localStorage
 $(function saveEvent() {
   $('.saveBtn').on('click', function() {
     localStorage.setItem($(this).parent().attr('id'), $(this).prev().val())
